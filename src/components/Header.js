78a734fdbdc04c1da5a5b9ef139668b7b836922d
BernhardP <Bernhard.Pajan@whizus.com>
2023-09-04 13:38:36 +0200

    feat: add whzlogo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
+import whzlogo from '../resources/images/logo.png'
+
 
 function Header(){
     return (
         <div className="fixed top-0 left-0 right-0 bg-white h-16 shadow-xl">
-
+            <img className='m-2 h-12 w-24' src={whzlogo} alt="whzlogo" />
         </div>
     )
 }

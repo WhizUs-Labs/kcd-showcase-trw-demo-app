ea6dca01cec3989f755937aa461f717cff444e60
BernhardP <Bernhard.Pajan@whizus.com>
2023-09-06 12:13:01 +0200

    feat: add revolvingcogs

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import whzlogo from '../resources/images/logo.png'
-
+import RevolvingCogs from "./RevolvingCogs";
 
 function Header(){
     return (
-        <div className="fixed top-0 left-0 right-0 bg-white h-32 shadow-xl">
-            <img className='my-2 mx-6 h-24 w-36' src={whzlogo} alt="whzlogo" />
+        <div>
+            <RevolvingCogs />
         </div>
     )
 }
--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,10 +1,14 @@
+import whzlogo from '../resources/images/logo.png';
+import './LandingPage.css';
 
 function LandingPage(){
     return (
         <div className="my-40 w-2/3 mx-auto" >
             <h1 className="text-6xl">Hello {`${process.env.REACT_APP_TEST}`}</h1>
+            <img className='my-2 mx-6 h-24 w-36' src={whzlogo} alt="whzlogo" />
         </div>
     )
 }
 
 export default LandingPage;
+

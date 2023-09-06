6e2f99a598fa7f2c35fddbeb0b5749de5c7e4345
Julian Zhuang <julian.zhuang@whizus.com>
2023-09-06 10:45:25 +0200

    git add ci config

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -2,7 +2,7 @@
 function LandingPage(){
     return (
         <div className="my-40 w-2/3 mx-auto" >
-            <h1 className="text-6xl">This is variable content for demonstration purposes!</h1>
+            <h1 className="text-6xl">Hello {`${process.env.REACT_APP_TEST}`}</h1>
         </div>
     )
 }

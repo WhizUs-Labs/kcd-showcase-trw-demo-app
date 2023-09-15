8807fe75a319b8b4e1cab2a4f224addba0b6bf0e
Thomas Stadler <thomas.stadler@whizus.com>
2023-09-15 15:29:06 +0200

    chore: fix tf syntax mistake

--- IaC/dns.tf
+++ IaC/dns.tf
@@ -6,5 +6,5 @@ resource "exoscale_domain_record" "my_host" {
   domain      = data.exoscale_domain.my_domain.id
   name        = "kcd"
   record_type = "A"
-  content     = data.exoscale_nlb.nlb.ip_address
+  content     = exoscale_nlb.nlb.ip_address
 }

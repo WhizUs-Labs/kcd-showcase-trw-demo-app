3bf519f953cc49969e48c641459514b41a81408f
Julian Zhuang <154322594+jwzhua@users.noreply.github.com>
2024-09-06 13:54:43 +0200

    TRW-396: load dynamical environment variables to react frontend (#16)
    * feat(TRW-396): fetch external config
    ---------
    
    Co-authored-by: Zhuang Julian <julian.zhuang@contractors.twinformatics.at>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Config from './utils/config'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const loadConfig = async () => {
+  try {
+      // Fetch config.json
+      const config_file = await fetch("/config.json");
+      const config = await config_file.json();
+
+      // map config in json to config class
+      Object.keys(config).forEach((key) => {
+          Config[key] = config[key];
+      });
+  } catch (e) {
+      console.log(e);
+  }
+};
+
+loadConfig().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
+
